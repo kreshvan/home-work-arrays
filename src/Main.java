8ba13bf8e4750f2387ebf8b62416d25e917521a1
днс <kreshvan12317@yandex.ru>
2024-06-02 09:20:14 +0300

    arrays, work on mistakes

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,38 @@
         public class Main {
             public static void main(String[] args) {
 //задание 1,1
-
-                int[] numbers = new int[12];
+                int[] numbers = new int[3];
                 numbers[0] = 1;
                 numbers[1] = 2;
                 numbers[2] = 3;
                 System.out.print(numbers[0]);
                 System.out.print(numbers[1]);
-                System.out.print(numbers[2]);
-                System.out.print(numbers[3]);
-                System.out.print(numbers[4]);
-                System.out.print(numbers[5]);
-                System.out.print(numbers[6]);
-                System.out.print(numbers[7]);
-                System.out.print(numbers[8]);
-                System.out.print(numbers[9]);
-                System.out.print(numbers[10]);
-                System.out.println(numbers[11]);
-                System.out.print(" ");
-                System.out.print(" ");
+                System.out.println(numbers[2]);
+
+
 
                 //задание 1.2
 
                 double[] numbers2 = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0};
                 System.out.println(numbers2[2]);
-                System.out.println(" ");
+
 
                 //задание 1.3
 
                 int[] box = {10, 9, 8};
                 System.out.println(box[0]);
-                System.out.println(" " + " ");
+
 
                 //задание 2
 
                 int[] numberss = new int[]{1, 2, 3};
-                for (int i = 0; i < 12; i++) {
-                    System.out.print(numbers[i]);
-                    if (i < 11) {
+                for (int i = 0; i < 3; i++) {
+                    System.out.print(numberss[i]);
+                    if (i < 3) {
                         System.out.print(", ");
                     }
                 }
-                System.out.println(" ");
+
 
 
                 //задание 2.1
@@ -54,12 +44,12 @@
 
                 for (int i = 0; i < 3; i++) {
                     System.out.print(numbersss[i]);
-                    if (i < 2) {
+                    if (i < 3) {
                         System.out.print(", ");
                     }
                 }
 
-                System.out.println(" ");
+
 
                 //задание 2.2
 
@@ -72,7 +62,6 @@
                     }
                 }
                 System.out.println(" ");
-                System.out.println(" ");
 
                 //задание 3
 
@@ -83,7 +72,6 @@
                         System.out.print(", ");
                     }
                 }
-                System.out.println(" ");
                 System.out.println(" ");
                 //задание 3.1
 
@@ -96,8 +84,7 @@
                         System.out.print(", ");
                     }
                 }
-                System.out.println(" ");
-                System.out.println(" ");
+
                 //задание 4
 
                 int[] numbers7 = new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3};
@@ -105,10 +92,7 @@
                     System.out.println((numbers7)[i]);
                     if (numbers7[i] % 2 != 0) {
                         numbers7[i] = numbers7[i] + 1;
-                    } else {
-                        numbers7[i] = 0;
                     }
-
                 }
                 System.out.println(Arrays.toString(numbers7));
             }
